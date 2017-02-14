acee03ece14480da8120cefdf622d4bd10c6c541
Jens Reimann <jreimann@redhat.com>
2017-02-14 14:47:16 +0100

    Allow to start a number of instances at the same time

--- src/test/java/org/eclipse/kapua/kura/simulator/TestApplication.java
+++ src/test/java/org/eclipse/kapua/kura/simulator/TestApplication.java
@@ -11,11 +11,16 @@
 package org.eclipse.kapua.kura.simulator;
 
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.logging.Level;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
 import org.eclipse.kapua.kura.simulator.app.Application;
 import org.eclipse.kapua.kura.simulator.app.annotated.AnnotatedApplication;
 import org.eclipse.kapua.kura.simulator.app.command.SimpleCommandApplication;
@@ -28,29 +33,76 @@ import org.slf4j.bridge.SLF4JBridgeHandler;
 public class TestApplication {
 	private static final Logger logger = LoggerFactory.getLogger(TestApplication.class);
 
-	public static void main(final String[] args) throws Exception {
+	public static void main(final String[] args) throws Throwable {
 
 		toInfinityAndBeyond();
 
+		final Options opts = new Options();
+		opts.addOption("n", "basename", true, "The base name of the simulator instance");
+		opts.addOption("c", "count", true, "The number of instances to start");
+		opts.addOption("b", "broker", true, "The URL to the broker");
+		opts.addOption("a", "account-name", true, "The name of the account");
+		opts.addOption("s", "shutdown", true, "Shutdown simulator after x seconds");
+
+		final CommandLine cli = new DefaultParser().parse(opts, args);
+
+		final String basename = cli.getOptionValue('n', "sim-");
+		final int count = Integer.parseInt(cli.getOptionValue('c', "1"));
+		final String broker = cli.getOptionValue('b', "tcp://kapua-broker:kapua-password@localhost:1883");
+		final String accountName = cli.getOptionValue('a', "kapua-sys");
+		final long shutdownAfter = Long.parseLong(cli.getOptionValue('s', Long.toString(Long.MAX_VALUE / 1_000L)));
+
 		logger.info("Starting ...");
 
-		final GatewayConfiguration configuration = new GatewayConfiguration(
-				"tcp://kapua-broker:kapua-password@localhost:1883", "kapua-sys", "sim-1");
+		logger.info("\tbasename : {}", basename);
+		logger.info("\tcount: {}", count);
+		logger.info("\tbroker: {}", broker);
+		logger.info("\taccount-name: {}", accountName);
 
 		final ScheduledExecutorService downloadExecutor = Executors
 				.newSingleThreadScheduledExecutor(new NameThreadFactory("DownloadSimulator"));
 
-		final Set<Application> apps = new HashSet<>();
-		apps.add(new SimpleCommandApplication(s -> String.format("Command '%s' not found", s)));
-		apps.add(AnnotatedApplication.build(new SimpleDeployApplication(downloadExecutor)));
+		final List<AutoCloseable> close = new LinkedList<>();
 
-		try (final MqttSimulatorTransport transport = new MqttSimulatorTransport(configuration);
-				final Simulator simulator = new Simulator(configuration, transport, apps);) {
-			Thread.sleep(120_000 * 1000);
+		try {
+			for (int i = 1; i <= count; i++) {
+
+				final String name = String.format("%s%s", basename, i);
+
+				final GatewayConfiguration configuration = new GatewayConfiguration(broker, accountName, name);
+
+				final Set<Application> apps = new HashSet<>();
+				apps.add(new SimpleCommandApplication(s -> String.format("Command '%s' not found", s)));
+				apps.add(AnnotatedApplication.build(new SimpleDeployApplication(downloadExecutor)));
+
+				final MqttSimulatorTransport transport = new MqttSimulatorTransport(configuration);
+				close.add(transport);
+				final Simulator simulator = new Simulator(configuration, transport, apps);
+				close.add(simulator);
+			}
+
+			Thread.sleep(shutdownAfter * 1_000L);
 			logger.info("Bye bye...");
 		} finally {
 			downloadExecutor.shutdown();
+
+			final LinkedList<Throwable> errors = new LinkedList<>();
+
+			for (final AutoCloseable c : close) {
+				try {
+					c.close();
+				} catch (final Exception e) {
+					errors.add(e);
+				}
+			}
+
+			final Throwable e = errors.pollFirst();
+			if (e != null) {
+				errors.forEach(e::addSuppressed);
+				throw e;
+			}
 		}
+
 		logger.info("Exiting...");
 	}
 
